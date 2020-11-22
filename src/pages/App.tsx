f87db0d56e6e68724e506923a567005b549acdcc
Jikun Liao <liaojikundever@gmail.com>
2020-11-22 09:50:16 -0600

    fix netlify deploy

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from 'components/Header';
 import Launchpad from './Launchpad';
 import RocketDetail from './RocketDetail';
 import Rockets from './Rockets';
