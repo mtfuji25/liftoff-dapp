8781e1765efc8173a9aaf88fd98929388f22bae9
Jikun Liao <liaojikundever@gmail.com>
2020-11-22 18:35:41 -0600

    wip: remove header for test deploy

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,12 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import Launchpad from './Launchpad';
 import RocketDetail from './RocketDetail';
 import Rockets from './Rockets';
-import Header from '../components/Header';
+// import Header from '../components/Header';
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Switch>
         <Route path={'/'} component={Launchpad} exact />
         <Route path={'/rockets'} component={Rockets} exact />
