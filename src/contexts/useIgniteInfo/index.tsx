34ca9ea921258231f9b5c19872adfa27a271fdb1
Jikun Liao <liaojikundever@gmail.com>
2021-01-05 11:57:48 -0600

    fix: useIgnitor

--- src/contexts/useIgniteInfo/index.tsx
+++ src/contexts/useIgniteInfo/index.tsx
@@ -3,10 +3,9 @@ import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import { Ignitor, Maybe } from 'utils/types';
 
-// NOTE: use tokenSaleId & wallet address instead of generating id because of address case sensitive
 const query = gql`
-  query GetIgniterInfo($tokenSale: string!, $address: string!) {
-    ignitors(where: { tokenSale: $tokenSale, address: $address }) {
+  query GetIgniterInfo($id: ID!) {
+    ignitor(id: $id) {
       id
       address
       ignited
@@ -17,29 +16,29 @@ const query = gql`
 `;
 
 type GraphResponse = {
-  ignitors: Ignitor[];
+  ignitor: Maybe<Ignitor>;
 };
 
 export const useIgniteInfo = (tokenSaleId: string, address: Maybe<string>) => {
   const [igniteInfo, setIgniteInfo] = useState<Maybe<Ignitor>>(null);
 
+  const id = `ignite_${tokenSaleId}_${address ? address.toLowerCase() : ''}`;
+
   const { data, error, loading } = useQuery<GraphResponse>(query, {
     notifyOnNetworkStatusChange: true,
     fetchPolicy: 'network-only',
     variables: {
-      tokenSale: tokenSaleId,
-      address: address
+      id
     }
   });
 
   useEffect(() => {
     if (
       data &&
-      data.ignitors &&
-      data.ignitors.length > 0 &&
-      data.ignitors[0].address.toLowerCase() === address?.toLowerCase()
+      data.ignitor &&
+      data.ignitor.address.toLowerCase() === address?.toLowerCase()
     ) {
-      setIgniteInfo(data.ignitors[0]);
+      setIgniteInfo(data.ignitor);
     } else {
       setIgniteInfo(null);
     }
