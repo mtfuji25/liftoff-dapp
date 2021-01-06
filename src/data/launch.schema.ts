58edaae5e719a149bab85c92d53723619b09e9c6
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-01-07 00:36:53 +0100

    Fix changes

---
+++ src/data/launch.schema.ts
@@ -0,0 +1,40 @@
+import { currentDate } from 'utils/date';
+import * as yup from 'yup';
+
+export const LaunchpadSchema = yup.object().shape({
+  projectName: yup.string().required('Project name is required'),
+  tokenTicker: yup.string().required('Token ticker is required'),
+  projectDescription: yup.string().required('Project description is required'),
+  logo: yup.mixed().required('Logo is required'),
+  openGraph: yup.mixed().required('Open Graph is required'),
+  websiteLink: yup
+    .string()
+    .matches(
+      /((https?):\/\/)?(www.)?[a-z0-9-]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#-]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      'Website URL is invalid'
+    )
+    .required('Website URL is required'),
+  dappLink: yup
+    .string()
+    .matches(
+      /((https?):\/\/)?(www.)?[a-z0-9-]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#-]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      'Dapp URL is invalid'
+    )
+    .required('Dapp URL is required'),
+  whitepaperLink: yup
+    .string()
+    .matches(
+      /((https?):\/\/)?(www.)?[a-z0-9-]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#-]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      'Whitepaper URL is invalid'
+    )
+    .required('Whitepaper URL is required'),
+  discord: yup.string(),
+  telegram: yup.string(),
+  twitter: yup.string(),
+  facebook: yup.string(),
+  date: yup.date().min(`${currentDate()}`).required('Date is required'),
+  time: yup.string().required('Launch time is required'),
+  softCap: yup.number().required('Soft cap is required'),
+  hardCap: yup.number().required('Hard cap is required'),
+  totalSupply: yup.number().required('Total supply is required')
+});
