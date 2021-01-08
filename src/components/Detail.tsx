3eef2d5c4f02ac97d01f810fa68dce819a09abaa
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2021-01-08 13:20:20 +0100

    break work to fix responsiveness

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -6,7 +6,7 @@ import {
   StyledRocketCard,
   TYPE,
   StatusBadge,
-  ExternalLink,
+  ExternalLink as UnstyledExternalLink,
   TBody,
   TRow,
   TData
@@ -21,6 +21,9 @@ import { ProjectConfig, TokenSale } from 'utils/types';
 import { Colors } from 'theme/styled';
 import { projectStatus } from 'utils';
 
+const ExternalLink = styled(UnstyledExternalLink)({
+  wordBreak: 'break-all'
+});
 const Card = styled(StyledRocketCard)`
   padding: 0;
 `;
