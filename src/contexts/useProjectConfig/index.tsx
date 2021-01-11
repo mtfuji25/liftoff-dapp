af1e4d8d1c441b7a55d71a75decaf34d59df9355
Carlsbad <carlsbad_sunshine@protonmail.com>
2021-01-11 13:50:32 -0800

    Fix ipfsHash by stripping non alphanumeric characters

--- src/contexts/useProjectConfig/index.tsx
+++ src/contexts/useProjectConfig/index.tsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react';
 import { ProjectConfig, Maybe } from 'utils/types';
 
-export const useProjectConfig = (ipfsHash: string | undefined) => {
+export const useProjectConfig = (ipfsHashRaw: string | undefined) => {
   const [projectConf, setProjectConf] = useState<Maybe<ProjectConfig>>(null);
 
   useEffect(() => {
-    if (!ipfsHash) {
+    if (!ipfsHashRaw) {
       setProjectConf(null);
       return;
     }
 
     const fetchConfig = async () => {
+      const ipfsHash = ipfsHashRaw.replace(/[^A-Za-z0-9]/g, '');
       const response = await fetch(`https://ipfs.io/ipfs/${ipfsHash}`);
+      console.log('response',response);
       const data: ProjectConfig = await response.json();
       setProjectConf(data);
     };
 
     fetchConfig();
-  }, [ipfsHash]);
+  }, [ipfsHashRaw]);
 
   return { projectConf };
 };
