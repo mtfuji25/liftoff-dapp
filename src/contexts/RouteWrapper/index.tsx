a6ba07cb2f592d850cbc4a09dc2e607c6220b7ef
Jikun Liao <liaojikundever@gmail.com>
2021-01-15 03:43:51 -0600

    fix: href in index.html, RouteWrapper

--- src/contexts/RouteWrapper/index.tsx
+++ src/contexts/RouteWrapper/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 export const RouterWrapper: React.FC = ({ children }) => {
-  return process.env.REACT_APP_ON_IPFS === 'true' ? (
-    <HashRouter>{children}</HashRouter>
-  ) : (
-    <BrowserRouter>{children}</BrowserRouter>
-  );
+  // return process.env.REACT_APP_ON_IPFS === 'true' ? (
+  //   <HashRouter>{children}</HashRouter>
+  // ) : (
+  //   <BrowserRouter>{children}</BrowserRouter>
+  // );
+
+  // NOTE: "homepage": "./", Due to this line in package.json, BrowserRouter not working, Use only HashRouter
+  return <HashRouter>{children}</HashRouter>;
 };
 
 export default RouterWrapper;
